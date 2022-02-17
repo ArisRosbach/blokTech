5fee1f138f6ccf20fc1909e0aa2ac1f2f47de678
ArisRosbach <aris.rosbach@hva.nl>
2022-02-17 13:59:21 +0100

    aanpassing index.js

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const camelCase = require('camelcase');
-const express = require('express')
-const app = express()
-const port = 3000
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
 
 console.log(camelCase('foo-bar'));
 console.log(camelCase('ik-ben-aris_rosbach'));
+
+
+// const express = require('express')
+// const app = express()
+// const port = 8000
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+const express = require('express')
+
+express()
+  .use('/static', express.static('static'))
+
+  .get('/', onhome)
+  .get('/about', onabout)
+  .get('/login', onlogin)
+
+  .listen(8000)
+
+function onhome(req, res) {
+  res.send('<h1>Hello World!</h1>')
+}
+
+function onabout(req, res) {
+  res.send('<h1>About me</h1>')
+}
+
+function onlogin(req, res) {
+  res.send('<h1>Login</h1>')
+}
