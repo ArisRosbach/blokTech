0aa06004360a2e9d38c51e19f7eed9deba418144
ArisRosbach <aris.rosbach@hva.nl>
2022-02-22 11:13:03 +0100

    poging 1 huiswerk week2 be

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
+// ----------------------------------- camelcase toepassen
+
 const camelCase = require('camelcase');
 
 console.log(camelCase('foo-bar'));
 console.log(camelCase('ik-ben-aris_rosbach'));
 
 
-// const express = require('express')
-// const app = express()
-// const port = 8000
-
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+// ----------------------------------- routes maken
 
 const express = require('express')
+const app = express()
+const PORT = 8000
 
-express()
-  .use('/static', express.static('static'))
+app.use('/static', express.static('static'))
 
-  .get('/', onhome)
-  .get('/about', onabout)
-  .get('/login', onlogin)
-
-  .listen(8000)
+app.get('/', onhome)
+app.get('/about', onabout)
+app.get('/login', onlogin)
+app.get('*', notfound)
 
 function onhome(req, res) {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Dit is de homepagina!</h1>')
 }
 
 function onabout(req, res) {
-  res.send('<h1>About me</h1>')
+  res.send('<h1>Hier vind je alles about me!</h1>')
 }
 
 function onlogin(req, res) {
-  res.send('<h1>Login</h1>')
+  res.send('<h1>Op deze pagina kun je inloggen</h1>')
 }
+
+function notfound(req, res) {
+  res.send('<h1>404 - Not Found!</h1>')
+}
+
+app.listen(PORT, function() {
+console.log('listening to port: ', PORT)
+})
+
+
+// ----------------------------------- render pages
+
+const handlebars = require('hbs');
+
+const hbs = handlebars.create({
+  defaultLayout: 'default',
+  extname: '.handlebars'
+})
+
+app.set('view engine', 'hbs');
+
+app.engine('hbs', handlebars.engine);
