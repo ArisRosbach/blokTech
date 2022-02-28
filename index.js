5e63c95bc56052c873ed195ba842348902c5cf02
ArisRosbach <aris.rosbach@hva.nl>
2022-02-28 10:55:56 +0100

    serving page be week 2

--- index.js
+++ index.js
@@ -6,11 +6,31 @@ console.log(camelCase('foo-bar'));
 console.log(camelCase('ik-ben-aris_rosbach'));
 
 
-// ----------------------------------- routes maken
+
 
 const express = require('express')
 const app = express()
 const PORT = 8000
+
+// ----------------------------------- render pages
+
+const exphbs = require("express-handlebars");
+app.engine(
+  "hbs",
+    exphbs.engine({
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+
+// ----------------------------------- routes maken
 
 app.use('/static', express.static('static'))
 
@@ -35,20 +55,8 @@ function notfound(req, res) {
   res.send('<h1>404 - Not Found!</h1>')
 }
 
-app.listen(PORT, function() {
-console.log('listening to port: ', PORT)
+app.listen(PORT, function () {
+  console.log('listening to port: ', PORT)
 })
 
 
-// ----------------------------------- render pages
-
-const handlebars = require('hbs');
-
-const hbs = handlebars.create({
-  defaultLayout: 'default',
-  extname: '.handlebars'
-})
-
-app.set('view engine', 'hbs');
-
-app.engine('hbs', handlebars.engine);
